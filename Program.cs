3daeb79fea559ae74ac5dfc67345265c23d35e3f
Sajeed <sajeedrs@gmail>
2020-12-17 01:46:53 +0530

    hello world app

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloApp
+{
+    class Program
+    { // This is first application  hello
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            
+        }
+    }
+}
